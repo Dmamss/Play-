071a03d2c06fa7aadd6580ceeb63a883a4cd1367
Dmamss <129612706+Dmamss@users.noreply.github.com>
2026-01-31 15:13:54 +0100

    Remove empty implementation methods from GSHandler
    Removed unimplemented methods from CGSHandler class.

--- Source/gs/GSHandler.cpp
+++ Source/gs/GSHandler.cpp
@@ -2212,22 +2212,3 @@ Framework::CBitmap CGSHandler::GetScreenshot()
 {
 	throw std::runtime_error("Screenshot feature is not implemented in current backend.");
 }
-void CGSHandler::InitializeImpl()
-{
-}
-
-void CGSHandler::ReleaseImpl()
-{
-}
-
-void CGSHandler::ProcessHostToLocalTransfer()
-{
-}
-
-void CGSHandler::ProcessLocalToHostTransfer()
-{
-}
-
-void CGSHandler::ProcessLocalToLocalTransfer()
-{
-}
